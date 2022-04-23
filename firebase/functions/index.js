295066ac26cf3d6a9649ab7f9e26f3b352d25a27
Aphinan <aphinan_th@kkumail.com>
2022-04-23 13:32:02 +0700

    user

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,15 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const cors = require("cors")();
+const app = express().use(cors);
+app.use(bodyParser.json());
 
+app.get("/", (_, res) => res.send("It's Work!"));
+app.use(routes.default);
+
+exports.apiV1 = functions.region("asia-southeast1").https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
